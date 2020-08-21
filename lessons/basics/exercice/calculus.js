58bff81215df99cc9a8294f7ada3eeaad4d6ff51
gkotulski <guillaume.kotulski@4d.com>
2020-08-21 17:05:50 +0200

    improve explanations

--- lessons/basics/exercice/calculus.js
+++ lessons/basics/exercice/calculus.js
@@ -1,15 +1,13 @@
 function calculus() {
-    let leftValue = parseInt(document.getElementById('a').value, 10)
-    let rightValue = parseInt(document.getElementById('b').value,10)
+    const leftValue = parseInt(document.getElementById('a').value, 10)
+    const rightValue = parseInt(document.getElementById('b').value, 10)
 
-    let operation = document.getElementById('operation').value
-
-    console.log(leftValue)
-    console.log(rightValue)
-    console.log(operation)
-    document.getElementById("result").innerText = compute(leftValue, rightValue, operation).toString()
+    const operation = document.getElementById('operation').value
+    document.getElementById('result').innerText = compute(leftValue, rightValue, operation).toString()
 }
+
 
 function compute(left, right, operation) {
     //return values depending on the operation
+    return 0
 }
---
+++ lessons/basics/exercice/readme.md
@@ -0,0 +1,11 @@
+# Exercise
+
+## What you will learn
+
+* Use variables
+* Use function
+* Use if/else
+
+## Instructions
+
+* Implement the different operations with the usage of functions.
